f71410a99651947ead59faa3711fcae1060ae21a
ebuenesy2 <ebuenesy2@gmail.com>
2022-05-23 13:57:03 +0300

    message

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -7,6 +7,13 @@ import ForumIcon from '@material-ui/icons/Forum';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
 import GroupIcon from '@material-ui/icons/Group';
+import InfoIcon from '@material-ui/icons/Info';
+import DeleteIcon from '@material-ui/icons/Delete';
+import GradeIcon from '@material-ui/icons/Grade';
+import DoneIcon from '@material-ui/icons/Done';
+import SendIcon from '@material-ui/icons/Send';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
 
 function Index() {
     
@@ -29,7 +36,93 @@ function Index() {
                             <div className="messageContainerRightTop">
                                 <div className="messageContainerRightTopProfileInfo">
                                       <img className="avatar" src="./assets/img/1.jpg"  alt="" />
+                                      <div className="messageContainerRightTopProfileInfoTitle">
+                                           <p className="nameSurname"> Olga Sukhorukova  </p>
+                                           <p className="status"> Typing...</p>
+                                      </div>
                                 </div>
+                                <div className="messageContainerRightTopIconBox">
+                                       <div className="messageContainerRightTopIcon"> <InfoIcon  style={{ color:"blue"}} className="icon" /> </div>
+                                       <div className="messageContainerRightTopIcon"> <DeleteIcon style={{ color:"red"}} className="icon" /> </div>
+                                       <div className="messageContainerRightTopIcon"> <GradeIcon  style={{ color:"#6a1b9a"}} className="icon" /> </div>
+                                       <div className="messageContainerRightTopIcon"> <DoneIcon  style={{ color:"#00695c"}} className="icon" /> </div>
+                                </div>
+                             </div>
+                             <hr />
+                             <div className="messageContainerRightBottom">
+                                  <ul className="chatBox">
+                                      <li className="chatLeft">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/1.jpg"  alt="" />
+                                              <div className="nameSurname" > Olga</div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      <li className="chatRight">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/2.jpg"  alt="" />
+                                              <div className="nameSurname" > Teresa </div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      <li className="divider"> Feb 18,2022 </li>
+                                      <li className="chatLeft">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/1.jpg"  alt="" />
+                                              <div className="nameSurname" > Olga</div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      <li className="chatRight">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/2.jpg"  alt="" />
+                                              <div className="nameSurname" > Teresa </div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      <li className="chatRight">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/2.jpg"  alt="" />
+                                              <div className="nameSurname" > Teresa </div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      <li className="chatRight">
+                                          <div className="profilInfo"> 
+                                              <img className="avatar" src="./assets/img/2.jpg"  alt="" />
+                                              <div className="nameSurname" > Teresa </div>
+                                          </div>
+                                          <div className="chat"> 
+                                               <div className="chatMessage" > There are many variations of passages of Lorem Ipsum available </div>
+                                               <div className="chatStatus" >  <p className="chatTime"> 08:45</p> <DoneIcon className="chatIcon" />  </div>
+                                          </div>
+                                      </li>
+                                      
+                                  </ul>
+                             </div>
+                             <div className="messageContainerRightBottomText">
+                                 <div className="messageContainerRightBottomTextTop">
+                                        <img className="avatar" src="./assets/img/2.jpg"  alt="" />
+                                         <AttachFileIcon style={{ color:"blue"}} className="icon" />
+                                         <SentimentSatisfiedAltIcon style={{ color:"blue"}} className="icon" />
+                                         <textarea placeholder="Send Message"></textarea>
+                                        <SendIcon style={{ color:"green"}} className="icon" />
+                                  </div>
                              </div>
                         </div>
                     </div>
