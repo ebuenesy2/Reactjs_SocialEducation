49a805719524b615a17a7a373c3746dbf978a265
ebuenesy2 <ebuenesy2@gmail.com>
2022-05-23 03:52:42 +0300

    html

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -2,6 +2,12 @@ import Navbar from "../../components/navbar";
 import Sidebar from "../../components/sidebar";
 import "./index.css"
 
+//! icon
+import ForumIcon from '@material-ui/icons/Forum';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import GroupIcon from '@material-ui/icons/Group';
+
 function Index() {
     
   return (
@@ -10,7 +16,22 @@ function Index() {
                <div className="messageHome">
                     <Navbar />
                     <div className="messageContainer">
-                        <p>  Deneme </p>
+                        <div className="messageContainerLeft">
+                            <div className="messageContainerLeftIconBox">
+                                  <div className="messageContainerLeftIcon"> <ForumIcon className="icon" /> </div>
+                                  <div className="messageContainerLeftIcon"> <AssignmentIndIcon className="icon" /> </div>
+                                  <div className="messageContainerLeftIcon"> <ContactMailIcon className="icon" /> </div>
+                                  <div className="messageContainerLeftIcon"> <GroupIcon className="icon" /> </div>
+                            </div>
+                            <div className="messageContainerLeftTitle"> Chats</div>
+                        </div>
+                        <div className="messageContainerRight">
+                            <div className="messageContainerRightTop">
+                                <div className="messageContainerRightTopProfileInfo">
+                                      <img className="avatar" src="./assets/img/1.jpg"  alt="" />
+                                </div>
+                             </div>
+                        </div>
                     </div>
                </div>
         </div>
