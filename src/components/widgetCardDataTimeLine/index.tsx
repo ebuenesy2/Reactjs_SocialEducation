ab151c0e3c495b0441218a0f76995a1947299ee8
ebuenesy2 <ebuenesy2@gmail.com>
2022-06-25 16:25:05 +0300

    dataTimeLine

--- src/components/widgetCardDataTimeLine/index.tsx
+++ src/components/widgetCardDataTimeLine/index.tsx
@@ -17,24 +17,24 @@ export const Index =(props: any) => {  //console.log("proops:",props);
 
     
   return (
-        <div className='widgetCardDataTimeLine'>
+        <div className='widgetCardDataTimeLine' style={{ backgroundColor: props.backgroundColor ? props.backgroundColor : "azure"  }} >
            <div className="widgetCardDataTimeLineContainer">
               <div className="widgetCardDataTimeLineBox">
-                 <div className="widgetCardDataTimeLineDetailTitle" > Tüm Veriler TimeLine </div>
+                 <div className="widgetCardDataTimeLineDetailTitle" style={{ color: props.colorTitle ? props.colorTitle : "black", fontSize: props.fontSizeTitle ? props.fontSizeTitle : "16px", fontWeight:props.fontWeightTitle ? props.fontWeightTitle : "700"  }} > {props.title ? props.title : "Title"} </div>
                <div className="widgetCardDataTimeLineDetailBox">
-                  <EjectIcon style={{ color: "darkgreen" }} />
-                  <span className="widgetCardDataTimeLineDetailBoxDescription"> 30 % </span>
-                  <span className="widgetCardDataTimeLineDetailBoxContent"> this month </span>
+                   <EjectIcon className={props.status=="negative" ? "widgetCardDataTimeLineDetailBoxStatus negative" : "widgetCardDataTimeLineDetailBoxStatus"} /> 
+                  <span  className={props.status=="negative" ? "widgetCardDataTimeLineDetailBoxDescription negative" : "widgetCardDataTimeLineDetailBoxDescription"}  style={{ fontSize: props.fontSizeDetailDescription ? props.fontSizeDetailDescription : "16px", fontWeight:props.fontWeightDetailDescription ? props.fontWeightDetailDescription : "700"  }} > {props.titleDetailDescription ? props.titleDetailDescription : "0%"} </span>
+                  <span className="widgetCardDataTimeLineDetailBoxContent" style={{ color: props.colorDetailContent ? props.colorDetailContent : "black", fontSize: props.fontSizeDetailContent ? props.fontSizeDetailContent : "16px", fontWeight:props.fontWeightDetailContent ? props.fontWeightDetailContent : "700"  }} > {props.titleDetailContent ? props.titleDetailContent : "This Month"} </span>
                </div>
 
                <div className="widgetCardDataTimeLineBottom" >
 
                   {Array(props.JsonData.length).fill(0).map((el, i) => 
                      <div className="widgetCardDataTimeLineBottomBox">
-                        <div className="widgetCardDataTimeLineBottomIconBox"> {props.JsonData[i].icon} </div>
+                        <div className="widgetCardDataTimeLineBottomIconBox" style={{ background: props.JsonData[i].backgroundColor ? props.JsonData[i].backgroundColor : "red" }}> {props.JsonData[i].icon} </div>
                      <div className="widgetCardDataTimeLineBottomInfoBox">
-                         <div className="widgetCardDataTimeLineBottomInfoBoxTitle"> {props.JsonData[i].title} </div>
-                         <div className="widgetCardDataTimeLineBottomInfoBoxDescription"> {props.JsonData[i].content} </div>
+                         <div className="widgetCardDataTimeLineBottomInfoBoxTitle" style={{ color: props.colorBoxTitle ? props.colorBoxTitle : "black", fontSize: props.fontSizeBoxTitle ? props.fontSizeBoxTitle : "16px", fontWeight:props.fontWeightBoxTitle ? props.fontWeightBoxTitle : "700"  }} > {props.JsonData[i].title} </div>
+                         <div className="widgetCardDataTimeLineBottomInfoBoxDescription" style={{ color: props.colorBoxDescription ? props.colorBoxDescription : "black", fontSize: props.fontSizeBoxDescription ? props.fontSizeBoxDescription : "16px", fontWeight:props.fontWeightBoxDescription ? props.fontWeightBoxDescription : "700"  }} > {props.JsonData[i].content} </div>
                      </div>                   
 
                      </div>
