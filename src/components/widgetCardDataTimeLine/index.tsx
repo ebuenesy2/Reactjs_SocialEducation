9e617d0b153418a5552339804ee106ce210b72ab
ebuenesy2 <ebuenesy2@gmail.com>
2022-06-25 21:01:16 +0300

    widgetCardDataTimeLine

--- src/components/widgetCardDataTimeLine/index.tsx
+++ src/components/widgetCardDataTimeLine/index.tsx
@@ -15,8 +15,7 @@ export const Index =(props: any) => {  //console.log("proops:",props);
            <div className="widgetCardDataTimeLineContainer">
               <div className="widgetCardDataTimeLineBox">
                  <div className="widgetCardDataTimeLineDetailTitle" style={{ color: props.colorTitle ? props.colorTitle : "black", fontSize: props.fontSizeTitle ? props.fontSizeTitle : "16px", fontWeight:props.fontWeightTitle ? props.fontWeightTitle : "700"  }} > {props.title ? props.title : "Title"} </div>
-               <div className="widgetCardDataTimeLineDetailBox">
-                 
+               <div className="widgetCardDataTimeLineDetailBox">                 
                    <EjectIcon className={props.status==="negative" ? "widgetCardDataTimeLineDetailBoxStatus negative" : "widgetCardDataTimeLineDetailBoxStatus"} /> 
                   <span  className={props.status==="negative" ? "widgetCardDataTimeLineDetailBoxDescription negative" : "widgetCardDataTimeLineDetailBoxDescription"}  style={{ fontSize: props.fontSizeDetailDescription ? props.fontSizeDetailDescription : "16px", fontWeight:props.fontWeightDetailDescription ? props.fontWeightDetailDescription : "700"  }} > {props.titleDetailDescription ? props.titleDetailDescription : "0%"} </span>
                   <span className="widgetCardDataTimeLineDetailBoxContent" style={{ color: props.colorDetailContent ? props.colorDetailContent : "black", fontSize: props.fontSizeDetailContent ? props.fontSizeDetailContent : "16px", fontWeight:props.fontWeightDetailContent ? props.fontWeightDetailContent : "700"  }} > {props.titleDetailContent ? props.titleDetailContent : "This Month"} </span>
