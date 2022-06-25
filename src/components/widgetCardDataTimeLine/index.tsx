8520331c730b77bd09ece15c40bab1d3db7addac
ebuenesy2 <ebuenesy2@gmail.com>
2022-06-25 16:29:53 +0300

    dataTimeLine

--- src/components/widgetCardDataTimeLine/index.tsx
+++ src/components/widgetCardDataTimeLine/index.tsx
@@ -34,7 +34,7 @@ export const Index =(props: any) => {  //console.log("proops:",props);
                         <div className="widgetCardDataTimeLineBottomIconBox" style={{ background: props.JsonData[i].backgroundColor ? props.JsonData[i].backgroundColor : "red" }}> {props.JsonData[i].icon} </div>
                      <div className="widgetCardDataTimeLineBottomInfoBox">
                          <div className="widgetCardDataTimeLineBottomInfoBoxTitle" style={{ color: props.colorBoxTitle ? props.colorBoxTitle : "black", fontSize: props.fontSizeBoxTitle ? props.fontSizeBoxTitle : "16px", fontWeight:props.fontWeightBoxTitle ? props.fontWeightBoxTitle : "700"  }} > {props.JsonData[i].title} </div>
-                         <div className="widgetCardDataTimeLineBottomInfoBoxDescription" style={{ color: props.colorBoxDescription ? props.colorBoxDescription : "black", fontSize: props.fontSizeBoxDescription ? props.fontSizeBoxDescription : "16px", fontWeight:props.fontWeightBoxDescription ? props.fontWeightBoxDescription : "700"  }} > {props.JsonData[i].content} </div>
+                         <div className="widgetCardDataTimeLineBottomInfoBoxDescription" style={{ color: props.colorBoxDescription ? props.colorBoxDescription : "black", fontSize: props.fontSizeBoxDescription ? props.fontSizeBoxDescription : "14px", fontWeight:props.fontWeightBoxDescription ? props.fontWeightBoxDescription : "400"  }} > {props.JsonData[i].content} </div>
                      </div>                   
 
                      </div>
