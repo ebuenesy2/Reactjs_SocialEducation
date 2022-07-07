4c7f6356d99ffd71e46254fc8f8845c3df192f83
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2022-07-07 13:45:59 +0300

    widgetFileTimeLine

--- src/components/widgetFileTimeLine/index.tsx
+++ src/components/widgetFileTimeLine/index.tsx
@@ -1,82 +1,47 @@
 import "./index.css"
 
-export const Index =(props: any) => {  //console.log("proops:",props);
+//! icon
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+
+export const Index =(props: any) => {  console.log("widgetFileTimeLine Props:",props);
 
 return ( 
    <>
       <div className='widgetFileTimeLine'>
          <div className="widgetFileTimeLineContainer">
             <div className="widgetFileTimeLineTitle" > File TimeLines </div>
-            <div className="widgetFileTimeLineDetails">
-               
-               <div className="widgetFileTimeLineBox" >
-                  <div className="widgetFileTimeLineHeader">
-                       <div className="widgetFileTimeLineHeaderBox"></div>
-                       <div className="widgetFileTimeLineHeaderTitle">11:03</div>
-                  </div>
-                  <div className="widgetFileTimeLineImageBoxes" >
-                     <div className="widgetFileTimeLineImageBox">
-                        <img className="widgetFileTimeLineImage" src="./assets/img/1.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/2.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/3.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/4.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/5.jpg" alt="" /> 
-                        
-                        <div className="widgetFileTimeLineImageOpacity" >+210</div>
+               <div className="widgetFileTimeLineDetails">
 
-                     </div>
-                     <div className="widgetFileTimeLineImageTitle" > 8 tane resim eklendi </div>
-                  </div>
-               </div>
+                  {Array(props.JsonData.length).fill(0).map((el, i) => 
+                     <div className="widgetFileTimeLineBox" >
+                        <div className="widgetFileTimeLineHeader">
+                           <div className="widgetFileTimeLineHeaderBox"></div>
+                           <div className="widgetFileTimeLineHeaderTitle"> {props.JsonData[i].time} </div>
+                        </div>
+                        <div className="widgetFileTimeLineFileBoxesFlex" >
+                           <div className="widgetFileTimeLineFileBoxes">
 
-               <div className="widgetFileTimeLineBox" >
-                  <div className="widgetFileTimeLineHeader">
-                       <div className="widgetFileTimeLineHeaderBox"></div>
-                       <div className="widgetFileTimeLineHeaderTitle">12:03</div>
-                  </div>
-                  <div className="widgetFileTimeLineImageBoxes" >
-                     <div className="widgetFileTimeLineImageBox">
-                        <img className="widgetFileTimeLineImage" src="./assets/img/1.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/2.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/3.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/4.jpg"  alt="" /> 
-                     </div>
-                     <div className="widgetFileTimeLineImageTitle" > 4 tane resim eklendi </div>
-                  </div>
-               </div>
+                              {Array(props.JsonData[i].data.length).fill(0).map((elj, j) => (  
+                                 j <= 4 ?
+                                    <div className="widgetFileTimeLineFileBox" >
+                                       <img className="widgetFileTimeLineFile" src={props.JsonData[i].data[j].imgUrl} alt="" />
+                                       {props.JsonData[i].data[j].type === "video" ?  <div className="widgetFileTimeLineFileOpacity" > <PlayArrowIcon /> </div>  :
+                                       props.JsonData[i].data[j].type === "voice" ?  <div className="widgetFileTimeLineFileOpacity" > <MusicNoteIcon /> </div>  :
+                                       j >= 4 ?  <div className="widgetFileTimeLineFileOpacity" > +{props.JsonData[i].data.length - 4} </div>  :
+                                       <div></div> }
+                                    </div>
+                                 :<div></div> 
 
-               <div className="widgetFileTimeLineBox" >
-                  <div className="widgetFileTimeLineHeader">
-                       <div className="widgetFileTimeLineHeaderBox"></div>
-                       <div className="widgetFileTimeLineHeaderTitle">15:03</div>
-                  </div>
-                  <div className="widgetFileTimeLineImageBoxes" >
-                     <div className="widgetFileTimeLineImageBox">
-                        <img className="widgetFileTimeLineImage" src="./assets/img/7.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/8.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/9.jpg"  alt="" /> 
-                     </div>
-                     <div className="widgetFileTimeLineImageTitle" > 3 tane resim eklendi </div>
-                  </div>
-               </div>
-                              
-               <div className="widgetFileTimeLineBox" >
-                  <div className="widgetFileTimeLineHeader">
-                       <div className="widgetFileTimeLineHeaderBox"></div>
-                       <div className="widgetFileTimeLineHeaderTitle">19:45</div>
-                  </div>
-                  <div className="widgetFileTimeLineImageBoxes" >
-                     <div className="widgetFileTimeLineImageBox">
-                        <img className="widgetFileTimeLineImage" src="./assets/img/14.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/15.jpg"  alt="" /> 
-                        <img className="widgetFileTimeLineImage" src="./assets/img/16.jpg"  alt="" /> 
-                     </div>
-                     <div className="widgetFileTimeLineImageTitle" > 3 tane video eklendi </div>
-                 </div>
-               </div>
-               
-            </div>
+                           ))}
 
+                           </div>
+                           <div className="widgetFileTimeLineFileTitle" > {props.JsonData[i].description} </div>
+                        </div>
+                     </div>
+                  )}
+                  
+               </div>
          </div>
       </div>
    </>
